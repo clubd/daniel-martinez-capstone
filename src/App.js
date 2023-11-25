445d37b1660ec3f620fa63826199e28cce354a42
clubd <danenrmar@gmail.com>
2023-11-25 18:55:07 -0500

    add loginPage and SignUpPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/SignUpPage/SignUpPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUp from "../../components/SignUp/SignUp";
+import "./SignUpPage.scss";
+
+function SignUpPage() {
+
+    return (
+        <div>
+            <SignUp />
+        </div>
+    );
+}
+
+export default SignUpPage;
