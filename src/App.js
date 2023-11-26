9bdc76eabc923d4647c9c4d23b3a8e7967cb6666
clubd <danenrmar@gmail.com>
2023-11-26 17:58:59 -0500

    remove homepage component add userInfo component and setup syntax layout for homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
+        <Route path="/homepage" element={<HomePage/>} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./UserInfo.scss";
+
+function UserInfo({ userData }) {
+    return (
+        <div className="user__info">
+            <img src={userData.avatar} alt="User Avatar" />
+            <p>{`${userData.firstName} ${userData.lastName}`}</p>
+        </div>
+    );
+}
+
+export default UserInfo;
