dd3720c254ad396d3cb1b784598746a00ec475f2
clubd <danenrmar@gmail.com>
2023-11-26 21:49:06 -0500

    syntax fix

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -9,7 +9,6 @@ function TaskList({ tasks }) {
                 <div key={task.id} className="task__list-item">
                     <div className="task__list-container">
                     <h3 className="task__title">{task.title}</h3>
-
                     <p className="task__description">{task.description}</p>
                     <Link className="task__link" to={`/task-list/${task.id}`}>Assign Task</Link>
                     </div>
