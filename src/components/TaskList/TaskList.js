126ab0af4e4839de56dacfd2bf6989f4b2afdc53
clubd <danenrmar@gmail.com>
2023-11-30 08:45:39 -0500

    add response to button and set timer to close modal itself

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,23 +1,68 @@
-
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./TaskList.scss";
 
 function TaskList({ tasks }) {
-    
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [alertMessage, setAlertMessage] = useState("");
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setAlertMessage("");
+    };
+
+    const handleAssignClick = () => {
+        openModal();
+    };
+
+    const handleYesClick = () => {
+        setAlertMessage("Task assigned to you!, Thank you");
+        setTimeout(() => {
+            closeModal();
+        }, 2000);
+    };
+
+    const handleNoClick = () => {
+        
+        closeModal();
+    };
+
     return (
         <>
-        <h2 className="task__heading">Tasks</h2>
-        <div className="task__list">
-            {tasks.map((task) => (
-                <div key={task.id} className="task__list-item">
-                    <div className="task__list-container">
-                    <h3 className="task__title">{task.title}</h3>
-                    <p className="task__description">{task.description}</p>
-                    <Link className="task__link" to={`/task-list/${task.id}`}>Assign Task</Link>
+            <h2 className="task__heading">Tasks</h2>
+            <div className="task__list">
+                {tasks.map((task) => (
+                    <div key={task.id} className="task__list-item">
+                        <div className="task__list-container">
+                            <h3 className="task__title">{task.title}</h3>
+                            <p className="task__description">{task.description}</p>
+                            <button className="task__link" onClick={handleAssignClick}>
+                                Assign Task
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                    {alertMessage ? (
+                            <p>{alertMessage}</p>
+                        ) : (
+                            <>
+                                <p>Do you want to assign this task to yourself?</p>
+                        <button className="modal-content__button" onClick={handleYesClick}>Yes</button>
+                        <button className="modal-content__button" onClick={handleNoClick}>No</button>
+                        </>
+                        )}
                     </div>
                 </div>
-            ))}
-        </div>
+            )}
         </>
     );
 }
